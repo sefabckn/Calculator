08bfade84b335a4ad47ac8ea9d9da0e39d2df8ff
SefaBckn <31974256+sefabckn@users.noreply.github.com>
2021-11-29 14:24:41 +0100

    html template created and styling completed. Functionality is still going on

---
+++ src/components/Calculator.js
@@ -0,0 +1,67 @@
+import { useReducer } from "react"
+
+import DigitButton from "./DigitButton"
+
+export const ACTIONS = {
+    ADD_DIGIT: "add-digit",
+    CHOOSE_OPERATION: "choose-operation",
+    CLEAR: "clear",
+    DELETE_DIGIT: "delete-digit",
+    EVALUATE: "evaluate",
+}
+
+
+
+const Calculator = () => {
+
+    
+    const reducer = (state, { type, payload }) => {
+        switch (type) {
+            case ACTIONS.ADD_DIGIT:
+                if (state.overwrite) {
+                    return {
+                        ...state,
+                        currentOperand: `${state.currentOperand || ''} ${payload.digit}`,
+                        overwrite: false
+                    }
+                }
+                if (payload.digit === '0' && state.currentOperand) {
+                    return state
+                }
+        }
+    }
+    const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
+
+    return (
+
+        <div className='calculator-grid'>
+            <div className='output'>
+                <div className='previous-operand'>{previousOperand}{operation}</div>
+                <div className='current-operand'>{currentOperand}</div>
+            </div>
+            <button className='span-two'>AC</button>
+            <button >DEL</button>
+            <button > ÷</button>
+            <DigitButton digit='1' dispatch={dispatch} />
+            <button >2</button>
+            <button >3</button>
+            <button >*</button>
+            <button >4</button>
+            <button >5</button>
+            <button >6</button>
+            <button >+</button>
+            <button >7</button>
+            <button >8</button>
+            <button >9</button>
+            <button >-</button>
+            <button >.</button>
+            <button >0</button>
+            <button className='span-two' >=</button>
+
+        </div>
+
+    )
+
+
+}
+export default Calculator
